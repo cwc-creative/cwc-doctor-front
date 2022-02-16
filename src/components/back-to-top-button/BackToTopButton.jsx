c85935520e4c2ed3269052f6024518ca96ae9d4a
Heriniaina Jean Bosco FANOMEZANTSOA <herifanomezantsoaf.com>
2022-02-16 16:26:27 +0100

    REFACTOR: BackToTopButton component

--- src/components/back-to-top-button/BackToTopButton.jsx
+++ src/components/back-to-top-button/BackToTopButton.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BackToTopButton = () => {
+
+  
+  // This function will scroll the window to the top 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
+
   return <div
-      data-moto-back-to-top-button=""
-      className="moto-back-to-top-button animated moto-back-to-top-button_visible"
-    >
-      <a ng-click="toTop($event)" className="moto-back-to-top-button-link">
-        <span className="moto-back-to-top-button-icon fa"></span>
-      </a>
-    </div>;
+        data-moto-back-to-top-button=""
+        className="moto-back-to-top-button animated moto-back-to-top-button_visible"
+      >
+        <a ng-click="toTop($event)" className="moto-back-to-top-button-link" onClick={scrollToTop}>
+          <span className="moto-back-to-top-button-icon fa"></span>
+        </a>
+      </div>;
+  
+  
 };
 
 export default BackToTopButton;
