0fac3abe707314a282929183262e495fe643283e
Heriniaina Jean Bosco FANOMEZANTSOA <herifanomezantsoaf.com>
2022-02-18 14:21:16 +0100

    REFACTOR: use props as parameter and add clicked state

--- src/components/widget-accordion/WidgetAccordion.jsx
+++ src/components/widget-accordion/WidgetAccordion.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const WidgetAccordion = ({id, children}) => {
-  return <div id={id}
+const WidgetAccordion = (props) => {
+
+    const [clicked, setClicked] = useState(false)
+    
+
+  return <div id={props.id}
             className="moto-widget moto-widget-accordion moto-widget_interactive moto-preset-default moto-spacing-top-medium moto-spacing-right-auto moto-spacing-bottom-auto moto-spacing-left-auto moto-disabling_row-fixed set-row-gutter-0"
             data-widget="accordion"
             data-preset="default"
-            data-close-others="">
+            data-close-others="" onClick={() => setClicked(!clicked) }>
                 <div
-                    id={`${id}__content`}
+                    id={`${props.id}__content`}
                     className="moto-widget-accordion__wrapper"
                 >
-                    {children}
+                    {props.children}
                 </div>
         </div>;
 };
