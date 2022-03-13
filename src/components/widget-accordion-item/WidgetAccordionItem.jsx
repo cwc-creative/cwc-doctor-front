b8ee1b6cd06aa5ea4a6cc278571c5950f602a8a9
Heriniaina Jean Bosco FANOMEZANTSOA <herifanomezantsoaf.com>
2022-03-13 12:58:25 +0100

    Update accordion item

--- src/components/widget-accordion-item/WidgetAccordionItem.jsx
+++ src/components/widget-accordion-item/WidgetAccordionItem.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WidgetText from '../widget-text/WidgetText';
 
-const WidgetAccordionItem = ({id, dataParentId, headerText, contentText, classes, displayContent, widgetTextId}) => {
-  return <div
+const WidgetAccordionItem = ({id, dataParentId, headerText, contentText, displayContent, widgetTextId}) => {
+    const [showText, setShowText] = useState(false);
+
+    let classes, class2 = '';
+
+    if (showText) {
+        classes = 'moto-widget-accordion__item_open';
+        displayContent = 'block';     
+    } else {
+        classes = 'moto-entertainment__playing_disabled moto-before-in-viewport_content-invisible';
+        class2 = 'moto-entertainment__animation_disabled';
+        displayContent = 'none';
+    }
+  
+    return <div
             id={id}
+            onClick={() => setShowText(!showText) }
             data-parent-id={dataParentId}
             className={`moto-widget moto-widget-accordion__item moto-widget_with-content-editor moto-widget-accordion__item_collapse-icon-right ${classes}`}
             data-widget="accordion.item"
@@ -26,7 +40,7 @@ const WidgetAccordionItem = ({id, dataParentId, headerText, contentText, classes
             >
                 <div
                     id={`${id}__content`}
-                    className="moto-widget__content moto-widget-accordion__content"
+                    className={`moto-widget__content moto-widget-accordion__content moto-entertainment__animation_ignore-visibility ${class2}`}
                 >
                     <WidgetText dataWidgetId={widgetTextId} dataSpacing="aaaa">
                         <p className="moto-text_normal">
