6a91f34a283919deaab7f0a2cc83bd5aeb2ecb29
Heriniaina Jean Bosco FANOMEZANTSOA <herifanomezantsoaf.com>
2022-03-13 13:10:36 +0100

    Create header component and insert it into home page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,97 @@
 import React from 'react';
+import WidgetBlock from '../widget-block/WidgetBlock';
+import WidgetContainer from '../widget-container/WidgetContainer';
+import WidgetRow from '../widget-row/WidgetRow';
+import WidgetRowColumn from '../widget-row__column/WidgetRowColumn';
+import WidgetSpacer from '../widget-spacer/WidgetSpacer';
 import NavDown from './NavDown';
 import NavUp from './NavUp';
 
+import HomeHeaderBg1 from '../../assets/images/cwc-doctor-home-header-bg1.png';
+import WidgetText from '../widget-text/WidgetText';
+
 const Header = () => {
-  return <>
-    <NavUp />
-    <NavDown />
-  </>;
+  return <WidgetBlock>
+
+                <NavUp />
+
+                <WidgetSpacer data_widget_id="wid_1561714957_exi5iztmo" />
+                <NavDown />
+                <WidgetSpacer data_widget_id="wid_1561714961_z71i16pht" />
+
+                <WidgetContainer containerContentId="5d15e1441" dataBgPosition="top">
+
+
+                    <WidgetRow 
+                        classes="row-fixed" 
+                        top="auto" bottom="auto"
+                        dataSpacing="aaaa"
+                        styles={{ 
+                            backgroundImage: `url(${HomeHeaderBg1})`,
+                            backgroundPosition: 'left bottom',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'contain'
+                        }}
+                        dataBgPosition="left bottom"
+                        dataBgImage="/src/assets/images/cwc-doctor-home-header-bg1.png"
+                    >
+                        <WidgetRowColumn classes="col-sm-12">
+
+                            <WidgetSpacer data_widget_id="wid_1561715235_r42quivjf" top="small" bottom="small" dataSpacing="sasa" visibleOn="tablet" />
+
+                            <WidgetRow dataGridType="xs" right="large" left="large" dataSpacing="alal">
+                                <WidgetRowColumn classes="col-xs-7">
+                                    <WidgetText bottom="small" dataSpacing="aasa">
+                                        <p className="moto-text_system_12">
+                                            BIENVENUE DANS NOTRE CENTRE DE SOINS MÉDICAUX
+                                        </p>
+                                        <p className="moto-text_system_12">&nbsp;</p>
+                                        <h1 className="moto-text_system_5">
+                                            Prenez soin de votre santé
+                                        </h1>
+                                    </WidgetText>
+                                </WidgetRowColumn>
+                                <WidgetRowColumn classes="col-xs-5" />
+                            </WidgetRow>
+
+                            <WidgetRow dataGridType="xs" right="large" left="large" dataSpacing="alal">
+                                <WidgetRowColumn classes="col-xs-7">
+                                    <WidgetText>
+                                        <p className="moto-text_system_9">
+                                            Vous pouvez facilement nous
+                                            joindre 24h/24 et 7j/7.<br />
+                                            {/* <ul>
+                                                <li><a href="tel:+261344058940">034 40 589 40</a></li>
+                                                <li><a href="tel:+261344158941">034 41 589 41</a></li>
+                                                <li><a href="tel:+261344258942">034 42 589 42</a></li>
+                                            </ul> */}
+                                        </p>
+                                    </WidgetText>
+                                    {/* <WidgetButton dataWidgetId="wid_1561715188_oqimtrg36" preset="2" top="medium">
+                                        <a
+                                            href="/#services"
+                                            data-action="page"
+                                            className="moto-widget-button-link moto-size-large moto-link"
+                                        >
+                                            <span className="fa moto-widget-theme-icon"></span>
+                                            <span className="moto-widget-button-label">
+                                                NOS SERVICES
+                                            </span>
+                                        </a>
+                                    </WidgetButton> */}
+                                </WidgetRowColumn>
+                                <WidgetRowColumn classes="col-xs-5" />
+                            </WidgetRow>            
+                          
+                            <WidgetSpacer data_widget_id="wid_1561715265_fggu126p9" top="large" bottom="large" dataSpacing="lala" visibleOn="tablet" />
+
+                        </WidgetRowColumn>
+
+                    </WidgetRow>
+                  
+
+                </WidgetContainer>
+            </WidgetBlock>;
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { InlineWidget } from 'react-calendly';
-import NavDown from '../components/header/NavDown';
-import NavUp from '../components/header/NavUp';
 import WidgetBlock from '../components/widget-block/WidgetBlock';
 import WidgetContainer from '../components/widget-container/WidgetContainer';
 import WidgetRow from '../components/widget-row/WidgetRow';
@@ -9,7 +7,7 @@ import WidgetRowColumn from '../components/widget-row__column/WidgetRowColumn';
 import WidgetSpacer from '../components/widget-spacer/WidgetSpacer';
 import MainLayout from '../layouts/main_layout';
 
-import HomeHeaderBg1 from '../assets/images/cwc-doctor-home-header-bg1.png';
+
 import HomeAppointment from '../assets/images/home-appointment.png';
 import HomeAppointmentBg1 from '../assets/images/home-appointment-bg1.png';
 import HomeDoctor from '../assets/images/home-doctor.jpg';
@@ -35,93 +33,14 @@ import WidgetAccordion from '../components/widget-accordion/WidgetAccordion';
 import WidgetAccordionItem from '../components/widget-accordion-item/WidgetAccordionItem';
 import WidgetDivider from '../components/widget-divider/WidgetDivider';
 import About from '../components/About';
+import Header from '../components/header/Header';
 
 const Home = () => {
   return <>
     <MainLayout>
-            <WidgetBlock>
-
-                <NavUp />
-
-                <WidgetSpacer data_widget_id="wid_1561714957_exi5iztmo" />
-                <NavDown />
-                <WidgetSpacer data_widget_id="wid_1561714961_z71i16pht" />
-
-                <WidgetContainer containerContentId="5d15e1441" dataBgPosition="top">
-
-
-                    <WidgetRow 
-                        classes="row-fixed" 
-                        top="auto" bottom="auto"
-                        dataSpacing="aaaa"
-                        styles={{ 
-                            backgroundImage: `url(${HomeHeaderBg1})`,
-                            backgroundPosition: 'left bottom',
-                            backgroundRepeat: 'no-repeat',
-                            backgroundSize: 'contain'
-                        }}
-                        dataBgPosition="left bottom"
-                        dataBgImage="/src/assets/images/cwc-doctor-home-header-bg1.png"
-                    >
-                        <WidgetRowColumn classes="col-sm-12">
-
-                            <WidgetSpacer data_widget_id="wid_1561715235_r42quivjf" top="small" bottom="small" dataSpacing="sasa" visibleOn="tablet" />
-
-                            <WidgetRow dataGridType="xs" right="large" left="large" dataSpacing="alal">
-                                <WidgetRowColumn classes="col-xs-7">
-                                    <WidgetText bottom="small" dataSpacing="aasa">
-                                        <p className="moto-text_system_12">
-                                            BIENVENUE DANS NOTRE CENTRE DE SOINS MÉDICAUX
-                                        </p>
-                                        <p className="moto-text_system_12">&nbsp;</p>
-                                        <h1 className="moto-text_system_5">
-                                            Prenez soin de votre santé
-                                        </h1>
-                                    </WidgetText>
-                                </WidgetRowColumn>
-                                <WidgetRowColumn classes="col-xs-5" />
-                            </WidgetRow>
-
-                            <WidgetRow dataGridType="xs" right="large" left="large" dataSpacing="alal">
-                                <WidgetRowColumn classes="col-xs-7">
-                                    <WidgetText>
-                                        <p className="moto-text_system_9">
-                                            Vous pouvez facilement nous
-                                            joindre 24h/24 et 7j/7.<br />
-                                            {/* <ul>
-                                                <li><a href="tel:+261344058940">034 40 589 40</a></li>
-                                                <li><a href="tel:+261344158941">034 41 589 41</a></li>
-                                                <li><a href="tel:+261344258942">034 42 589 42</a></li>
-                                            </ul> */}
-                                        </p>
-                                    </WidgetText>
-                                    {/* <WidgetButton dataWidgetId="wid_1561715188_oqimtrg36" preset="2" top="medium">
-                                        <a
-                                            href="/#services"
-                                            data-action="page"
-                                            className="moto-widget-button-link moto-size-large moto-link"
-                                        >
-                                            <span className="fa moto-widget-theme-icon"></span>
-                                            <span className="moto-widget-button-label">
-                                                NOS SERVICES
-                                            </span>
-                                        </a>
-                                    </WidgetButton> */}
-                                </WidgetRowColumn>
-                                <WidgetRowColumn classes="col-xs-5" />
-                            </WidgetRow>            
-                          
-                            <WidgetSpacer data_widget_id="wid_1561715265_fggu126p9" top="large" bottom="large" dataSpacing="lala" visibleOn="tablet" />
-
-                        </WidgetRowColumn>
-
-                    </WidgetRow>
-                  
-
-                </WidgetContainer>
-            </WidgetBlock>
+            
     
-    
+    <Header />
     <About />
     
     <WidgetBlock top="large" bottom="medium" dataSpacing="lama">
